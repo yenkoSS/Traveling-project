8e5675cc3170e453672c1af6a9a2d96b1b6e0924
Stefan Petru-Cornel <petrucornel.stefan@gmail.com>
2023-09-25 12:10:15 +0300

    added smooth scrolling effect on nav-link buttons

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('app.js - ok')
+document.querySelector('#btn-more').addEventListener('click', function() {
+    document.querySelector('#section-about').scrollIntoView({behavior: 'smooth'})
+})
+
+
+document.querySelectorAll('.nav-link').forEach(function(element) {
+
+    element.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const sectionID = element.getAttribute('href')
+
+        document.querySelector(sectionID).scrollIntoView({behavior: 'smooth'});
+    })
+})
